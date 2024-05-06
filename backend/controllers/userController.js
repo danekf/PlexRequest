f57948daa45ece2aac3ba64f553b24c3227e76f9
Danek <danek.fill@outlook.com>
2024-05-06 15:01:41 -0400

    add bcrypt to user registration and login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,41 +1,49 @@
 const User = require('../schemas/userSchema');
+const bcrypt = require('bcrypt');
 
 const loginUser = async (req, res) => {
-
-  const {username, password} = req.data;
-  //TODO : add bcrypt compare/other encryption
-  const passwordHash = password;
+  const { username, password } = req.body;
 
   try {
-    const user = await User.find({
-      username: username,
-      passwordHash: passwordHash,
-    });
-    //TODO : Set cookie on user
-    res.status(200).json(user);
+    const user = await User.findOne({ username: username }); 
+    if (!user) {
+      return res.status(401).json({ error: 'Incorrect username or password' });
+    }
+
+    const correctPassword = await bcrypt.compare(password, user.passwordHash); 
+    if (correctPassword) {
+      res.status(200).json({ message: 'Login Successful' });
+    } else {
+      res.status(401).json({ error: 'Incorrect username or password' }); 
+    }
+
   } catch (error) {
-    res.status(400).json({error: error.message});
+    res.status(500).json({ error: error.message });
   }
 }
 
 const registerUser = async (req, res) => {
-  const {username, password, email} = req.data;
-  //TODO : add bcrypt compare/other encryption
-  const passwordHash = password;
+  const { username, password, email } = req.body;
 
+  //bcrypt
   try {
+    const saltRounds = 10; 
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
     const user = await User.create({
       username: username,
       passwordHash: passwordHash, 
       email: email,
       isAdmin: false,
     });
-    //TODO : Set cookie on user
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(400).json({error: error.message});
-  }
 
+    res.status(201).json({ message: 'User Created' });
+  } catch (error) {
+    if (error.code === 11000) { // MongoDB duplicate key error
+      return res.status(400).json({ error: 'Username or email already exists' });
+    }
+    res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = {
